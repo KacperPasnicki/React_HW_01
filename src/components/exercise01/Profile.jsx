cea32d7d89e9f549e136693923e2040c173911b0
KacperPasnicki <kacper.pas@hotmail.com>
2022-10-09 14:09:13 +0200

    exercise 1 is done

---
+++ src/components/exercise01/Profile.jsx
@@ -0,0 +1,46 @@
+import ProfileCss from './Profile.module.css'
+
+export const UserData = 
+({
+    "username": "Jacques Gluke",
+    "tag": "jgluke",
+    "location": "Ocho Rios, Jamaica",
+    "avatar": "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+    "stats": {
+      "followers": 5603,
+      "views": 4827,
+      "likes": 1308
+    }
+  })
+
+ export const Profile = props =>
+ (<div class={ProfileCss.profile}>
+  <div class={ProfileCss.description}>
+    <img
+      src={UserData.avatar}
+      alt="User avatar"
+      class={ProfileCss.img}
+      width="140"/>
+    <p class={ProfileCss.username}>{UserData.username}</p>
+    <p class={ProfileCss.tag}>{UserData.tag}</p>
+    <p class={ProfileCss.location}>{UserData.location}</p>
+  </div>
+
+  <ul class={ProfileCss.stats}>
+    <li>
+      <span class={ProfileCss.label}>Followers</span>
+      <span class={ProfileCss.quantity}>{UserData.stats.followers}</span>
+    </li>
+    <li>
+      <span class={ProfileCss.label}>Views</span>
+      <span class={ProfileCss.quantity}>{UserData.stats.views}</span>
+    </li>
+    <li>
+      <span class={ProfileCss.label}>Likes</span>
+      <span class={ProfileCss.quantity}>{UserData.stats.likes}</span>
+    </li>
+  </ul>
+</div>)
+
+
+
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { Profile } from './components/exercise01/Profile'
+import { UserData } from './components/exercise01/Profile'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Profile />
   </React.StrictMode>
 );
+
+
