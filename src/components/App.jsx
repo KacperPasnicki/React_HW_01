f8bb065c8b6811ca36531c3219c779e64bb086ad
KacperPasnicki <kacper.pas@hotmail.com>
2022-10-14 00:14:23 +0200

    add components into APP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
 import PropTypes from 'prop-types';
 
-export const App = (HW) => {
+export const App = ({children}) => {
   return (
     <div
       style={{
-        // height: '100vh',
-        // display: 'flex',
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // fontSize: 40,
-        // color: '#010101',
-        // opacity: 0.1
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        opacity: 0.1
       }}
     >
       React homework template
-      {HW}
+      {children}
     </div>
   );
-};
+    }
 
 // App.propTypes = {
 
